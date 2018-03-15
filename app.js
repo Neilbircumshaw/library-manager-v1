fa4b6eadfcd692a2f702eda7781dcf0e845ec30b
Neilbircumshaw <n.bircumshaw@hotmail.co.uk>
2018-03-15 23:53:07 +0000

    added ./model

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const sequelize = require("/models").sequelize;
+const sequelize = require("./models").sequelize;
 sequelize.sync().then(function(){
 app.listen(3000);
 });
